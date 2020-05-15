97b6877b00d0d1c10f713affbef0d2a45e40d5da
xXHachimanXx <globaldep90@gmail.com>
2020-05-15 18:33:48 -0300

    Implementando funções para pegar o primeiro e o segundo menor caminho no Branch and Bound.

--- src/algorithms/branchAndBound.cpp
+++ src/algorithms/branchAndBound.cpp
@@ -18,8 +18,44 @@ void BranchAndBound::atualizarMelhorCaminho(int* caminhoParcial)
     this->cities[vertices] = caminhoParcial[0];    
 }
 
+int BranchAndBound::primeiroMenor(int vertice) 
+{
+    int menor = MAX;
+    int vertices = this->graph.getV();
+    double** matriz = this->graph.getGraph();
+
+    for(int x = 0; x < vertices; x++)
+    {        
+        if(vertice != x && matriz[vertice][x] < menor)
+            menor = matriz[vertice][x];
+    }
+    return menor; 
+}
+
+int BranchAndBound::segundoMenor(int vertice) 
+{
+    int primeiro_menor = MAX, segundo_menor = MAX;
+    int vertices = this->graph.getV();
+    double** matriz = this->graph.getGraph();
+
+    for (int x = 0; x < vertices; x++)
+    {
+        if(x != vertice && matriz[vertice][x] <= primeiro_menor)
+        {
+            segundo_menor = primeiro_menor;
+            primeiro_menor = matriz[vertice][x];
+        }
+        else
+        {
+            if(matriz[vertice][x] <= segundo_menor && matriz[vertice][x] != primeiro_menor)
+                segundo_menor = matriz[vertice][x];
+        }
+    }
+    return segundo_menor;
+}
+
 void BranchAndBound::branchAndBound(
-    int parcial, int nivel, int *caminhoParcial, bool *visitados, int menorCaminho)
+    int parcial, int nivel, int limite_atual, int *caminhoParcial, bool *visitados, int menorCaminho)
 {
     int vertices = this->graph.getV();
     double **matriz = this->graph.getGraph();
@@ -51,18 +87,25 @@ void BranchAndBound::branchAndBound(
             // Se a cidade não foi visitada e tem caminho entre os vertices
             if (!visitados[x] && matriz[nivelAnterior][x] != 0)
             {
+                int aux = limite_atual;
                 parcial += matriz[nivelAnterior][x];
+
+                if(nivel == 1)
+                    limite_atual -= primeiroMenor(caminhoParcial[nivel-1]) + primeiroMenor(x)/2;
+                else
+                    limite_atual -= segundoMenor(caminhoParcial[nivel-1]) + primeiroMenor(x)/2;
 
                 // Se o caminho parcial já é maior que o menor
                 // caminho atual, então podemos descartar esta computação.
-                if (parcial < menorCaminho)
+                if (parcial + limite_atual < menorCaminho)
                 {
                     caminhoParcial[nivel] = x;
                     visitados[x] = true;
-                    branchAndBound(parcial, nivel + 1, caminhoParcial, visitados, menorCaminho);
+                    branchAndBound(parcial, nivel + 1, limite_atual, caminhoParcial, visitados, menorCaminho);
                 }
 
                 parcial -= matriz[nivelAnterior][x];
+                limite_atual = aux;
 
                 memset(visitados, false, vertices);
                 for (int y = 0; y < nivel; y++)
@@ -79,14 +122,19 @@ void BranchAndBound::run()
     int *melhorCaminho = new int[vertices + 1];
     bool *visitados = new bool[vertices];
     int menorCaminho = MAX;
+    int limite_atual = 0;
 
     memset(caminhoParcial, -1, vertices + 1);
     memset(melhorCaminho, -1, vertices + 1);
     memset(visitados, false, vertices);
 
+    for (int x = 0; x < vertices; x++)
+        limite_atual += (primeiroMenor(x) + segundoMenor(x));
+    limite_atual = (limite_atual & 1)? limite_atual/2 + 1 : limite_atual/2;
+
     visitados[0] = true;
     caminhoParcial[0] = 0;
 
-    branchAndBound(0, 1, caminhoParcial, visitados, menorCaminho);
+    branchAndBound(0, 1, limite_atual, caminhoParcial, visitados, menorCaminho);
     showResult();
 }
--- src/algorithms/branchAndBound.h
+++ src/algorithms/branchAndBound.h
@@ -12,5 +12,7 @@ public:
 
     void atualizarMelhorCaminho(int* caminhoParcial);
     void branchAndBound(
-        int parcial, int nivel, int *caminhoParcial, bool *visitados, int menorCaminho);
+        int parcial, int nivel, int limite_atual, int *caminhoParcial, bool *visitados, int menorCaminho);
+    int primeiroMenor(int vertice);
+    int segundoMenor(int vertice);
 };
