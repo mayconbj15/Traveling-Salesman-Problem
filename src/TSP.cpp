e60e31274819f2466cd1562a59865909bcc156d4
axell-brendow <breno.axel@gmail.com>
2020-05-16 01:51:16 -0300

    Make time expression more portable

--- src/TSP.cpp
+++ src/TSP.cpp
@@ -38,7 +38,7 @@ double TSP::runAndCountTime()
 
     end = clock();
 
-    time = double(end - start);
+    time = double(end - start) / CLOCKS_PER_SEC * 1000;
     totalTime += time;
     showResult();
     printClock();
