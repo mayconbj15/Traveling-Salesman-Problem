e0ed2e334f23fb988885c522c0f49c99060a8cc0
axell-brendow <breno.axel@gmail.com>
2020-05-16 16:24:13 -0300

    Add gdb flag to g++

--- make.ps1
+++ make.ps1
@@ -4,7 +4,7 @@ $files = $(Get-ChildItem algorithms/*.cpp -name | foreach {
     echo "algorithms/$_"
 })
 
-g++ -o main.exe `
+g++ -g -o main.exe `
     -std=c++14 `
     -I algorithms $files `
     main.cpp TSP.cpp graph.cpp
