ed4fc7e1d0fb2c6690c51e950cf89458bb0fc592
xXHachimanXx <globaldep90@gmail.com>
2020-05-16 21:59:13 -0300

    Corrigindo erro no Branch and Bound: resetar atributo de menor distância sempre que executar novamente.

--- src/algorithms/branchAndBound.cpp
+++ src/algorithms/branchAndBound.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <string.h>
+#include <stdio.h>
 
 #include "../graph.h"
 #include "../constants.h"
@@ -13,62 +14,25 @@ BranchAndBound::BranchAndBound(Graph &&graph) : TSP(graph, "Branch and Bound") {
 
 void BranchAndBound::atualizarMelhorCaminho(int *caminhoParcial)
 {
-    int vertices = this->graph.getV();
+    int vertices = this->graph.getV();    
     for (int x = 0; x < vertices; x++)
         this->cities[x] = caminhoParcial[x];
 }
 
-int BranchAndBound::primeiroMenor(int vertice)
-{
-    int menor = MAX;
-    int vertices = this->graph.getV();
-    double **matriz = this->graph.getGraph();
-
-    for (int x = 0; x < vertices; x++)
-    {
-        if (vertice != x && matriz[vertice][x] < menor)
-            menor = matriz[vertice][x];
-    }
-    return menor;
-}
-
-int BranchAndBound::segundoMenor(int vertice)
-{
-    int primeiro_menor = MAX, segundo_menor = MAX;
-    int vertices = this->graph.getV();
-    double **matriz = this->graph.getGraph();
-
-    for (int x = 0; x < vertices; x++)
-    {
-        if (x != vertice && matriz[vertice][x] <= primeiro_menor)
-        {
-            segundo_menor = primeiro_menor;
-            primeiro_menor = matriz[vertice][x];
-        }
-        else
-        {
-            if (matriz[vertice][x] <= segundo_menor && matriz[vertice][x] != primeiro_menor)
-                segundo_menor = matriz[vertice][x];
-        }
-    }
-    return segundo_menor;
-}
-
 void BranchAndBound::branchAndBound(
-    int parcial, int nivel, int *caminhoParcial, bool *visitados)
+    double** matriz, double parcial, int nivel, int *caminhoParcial, bool *visitados)
 {
-    int vertices = this->graph.getV();
-    double **matriz = this->graph.getGraph();
+    int vertices = this->graph.getV();    
     int nivelAnterior = caminhoParcial[nivel - 1];
-
+    // printf("%lf \n", this->distance);
     // Se chegar no fim da árvore
     if (nivel == vertices)
     {
         // Testata se tem caminho direto de volta para
         // o vértice inicial
-        if (matriz[nivelAnterior][caminhoParcial[0]] != 0)
+        if (matriz[nivelAnterior][caminhoParcial[0]] != UNDEFINED)
         {
-            int resultadoAtual = parcial + matriz[nivelAnterior][caminhoParcial[0]];
+            double resultadoAtual = parcial + matriz[nivelAnterior][caminhoParcial[0]];
 
             // Se o caminho percorrido até o fim da árvore é
             // menor que o menor atual, então podemos atribuí-lo
@@ -87,7 +51,7 @@ void BranchAndBound::branchAndBound(
         for (int x = 0; x < vertices; x++)
         {
             // Se a cidade não foi visitada e tem caminho entre os vertices
-            if (!visitados[x] && matriz[nivelAnterior][x] != 0)
+            if (!visitados[x] && matriz[nivelAnterior][x] != UNDEFINED)
             {
                 parcial += matriz[nivelAnterior][x]; // salvar caminho anterior
                             
@@ -97,7 +61,7 @@ void BranchAndBound::branchAndBound(
                 {
                     caminhoParcial[nivel] = x;
                     visitados[x] = true;
-                    branchAndBound(parcial, nivel + 1, caminhoParcial, visitados);
+                    branchAndBound(matriz, parcial, nivel + 1, caminhoParcial, visitados);
                 }
 
                 parcial -= matriz[nivelAnterior][x];
@@ -112,10 +76,11 @@ void BranchAndBound::branchAndBound(
 
 void BranchAndBound::run()
 {
+    this->distance = MAX;
     int vertices = this->graph.getV();
-    int menorCaminho = MAX;
+    double **matriz = this->graph.getGraph();
     int *caminhoParcial = new int[vertices + 1];
-    bool *visitados = new bool[vertices];
+    bool *visitados = new bool[vertices];    
 
     memset(caminhoParcial, -1, vertices + 1);
     memset(visitados, false, vertices);    
@@ -123,5 +88,8 @@ void BranchAndBound::run()
     visitados[0] = true;
     caminhoParcial[0] = 0;
 
-    branchAndBound(0, 1, caminhoParcial, visitados);
+    branchAndBound(matriz, 0, 1, caminhoParcial, visitados);
+    delete caminhoParcial;
+    delete visitados;
+    free(matriz);
 }
--- src/algorithms/branchAndBound.h
+++ src/algorithms/branchAndBound.h
@@ -14,7 +14,7 @@ public:
 
     void atualizarMelhorCaminho(int* caminhoParcial);
     void branchAndBound(
-        int parcial, int nivel, int *caminhoParcial, bool *visitados);
+        double **matriz, double parcial, int nivel, int *caminhoParcial, bool *visitados);
     int primeiroMenor(int vertice);
     int segundoMenor(int vertice);
 };
