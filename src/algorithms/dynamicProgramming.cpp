f321459a5fbbc0936323e726b494f4499c5dc111
axell-brendow <breno.axel@gmail.com>
2020-05-23 15:45:42 -0300

    Document genetic and dynamic algorithms

--- src/algorithms/dynamicProgramming.cpp
+++ src/algorithms/dynamicProgramming.cpp
@@ -25,8 +25,29 @@ DynamicProgramming::DynamicProgramming(Graph &&graph)
 void getSolution(vector<vector<int>> &memoTable, int citiesSubset, int endingCity,
                  int numCities, int *cities);
 
+/**
+ * @brief Checa se uma cidade não está no subconjunto.
+ *
+ * @param city Índice da cidade a ser checada. Considerando que a city 0 seria a
+ * última à direita (Little Endian).
+ * @param subset Subconjunto de cidades. Ex.: subset = 14 = 1110 (cidades 1, 2 e 3)
+ *
+ * @return true caso a cidade não esteja no subconjunto. Ex.: notIn(14, 0) = true.
+ * @return false caso contrário.
+ */
 bool notIn(int subset, int city) { return ((1 << city) & subset) == 0; }
 
+/**
+ * @brief Remove uma cidade do subconjunto de cidades. Um subconjunto é uma sequência
+ * de bits.
+ *
+ * @param city Índice da cidade a ser removida. Considerando que a city 0 seria a
+ * última à direita (Little Endian).
+ * @param subset Subconjunto de cidades. Ex.: subset = 14 = 1110 (cidades 1, 2 e 3)
+ *
+ * @return int O novo subconjunto com a cidade removida.
+ * Ex.: removeCity(2, 14) = 10 = 1010
+ */
 int removeCity(int city, int subset) { return subset ^ (1 << city); }
 
 /**
@@ -68,12 +89,37 @@ int dynamicTSP(vector<vector<int>> &memoTable, double **graph, int citiesSubset,
     return memoTable[newSubset][endingCity] = bestCost;
 }
 
+/**
+ * @brief Populate memoTable with know values from the graph. For example, for the
+ * empty cities subset (0), if I consider the ending city as 1, then, the cost to
+ * pass through this path is the cost to go from city 1 to city 0 that is the
+ * starting and ending city.
+ *
+ * @param memoTable Table were the rows are acessed via an integer number that
+ * represents the subset of cities (Ex.: 1110 = 14 would represent a set with
+ * cities 1, 2 and 3. Each 1 represent a city in the set) and the columns are
+ * also acessed via an integer number that represents the desired ending city.
+ * So, memoTable[14][0] will represent the minimum cost to reach city 0 passing
+ * through the cities in the subset {1, 2, 3}.
+ * @param graph Hamiltonian graph.
+ * @param numCities Total number of cities.
+ */
 void initMemoTable(vector<vector<int>> &memoTable, double **graph, int numCities)
 {
     for (size_t city = 1; city < numCities; city++)
         memoTable[0][city] = graph[city][0];
 }
 
+/**
+ * @brief Solve the Travelling Salesman Problem using Dynamic Programming.
+ *
+ * @param graph Given graph.
+ * @param numCities Number of cities (number of vertices of the graph).
+ * @param cities Pointer to an array to store the sequence of cities of the best
+ * solution.
+ *
+ * @return int The cost of the best solution.
+ */
 int dynamicTSP(double **graph, int numCities, int *cities)
 {
     // 1 << numCities = 2^numCities
@@ -94,6 +140,21 @@ void DynamicProgramming::run()
     distance = dynamicTSP(graph.getGraph(), graph.getV(), cities.get());
 }
 
+/**
+ * @brief Discover what is the best solution Dynamic Programming found.
+ *
+ * @param memoTable Table were the rows are acessed via an integer number that
+ * represents the subset of cities (Ex.: 1110 = 14 would represent a set with
+ * cities 1, 2 and 3. Each 1 represent a city in the set) and the columns are
+ * also acessed via an integer number that represents the desired ending city.
+ * So, memoTable[14][0] will represent the minimum cost to reach city 0 passing
+ * through the cities in the subset {1, 2, 3}.
+ * @param citiesSubset The subset of cities this function will analyze.
+ * @param endingCity A city in the subset that will be the last in the path.
+ * @param numCities Total number of cities.
+ * @param cities Pointer to an array to store the sequence of cities of the best
+ * solution.
+ */
 void getSolution( // Down the tree getting the best path after dynamic
                   // programming
     vector<vector<int>> &memoTable, int citiesSubset, int endingCity, int numCities,
--- src/algorithms/geneticAlgorithm.cpp
+++ src/algorithms/geneticAlgorithm.cpp
@@ -17,6 +17,13 @@ GeneticAlgorithm::GeneticAlgorithm(Graph &&graph) : TSP(graph, "Genetic Algorith
 {
 }
 
+/**
+ * @brief Cria os índices onde o indivíduo será mutado.
+ *
+ * @param mutationIndex0 Referência para a variável que guardará o primeiro índice.
+ * @param mutationIndex1 Referência para a variável que guardará o segundo índice.
+ * @param numVertices Número de vértices do grafo.
+ */
 void createMutationIndexes(int &mutationIndex0, int &mutationIndex1, int numVertices)
 {
     mutationIndex0 = 1 + rand() % (numVertices - 1);
@@ -25,6 +32,13 @@ void createMutationIndexes(int &mutationIndex0, int &mutationIndex1, int numVert
         mutationIndex1 = 1 + rand() % (numVertices - 1);
 }
 
+/**
+ * @brief Muta um indivíduo trocando dois vértices de lugar.
+ *
+ * @param newPath Ponteiro para o novo caminho desse indivíduo.
+ * @param mutationIndex0 Índice do primeiro vértice a ser trocado de lugar.
+ * @param mutationIndex1 Índice do segundo vértice a ser trocado de lugar.
+ */
 void mutate(int *newPath, int mutationIndex0, int mutationIndex1)
 {
     int aux = newPath[mutationIndex0];
@@ -32,6 +46,13 @@ void mutate(int *newPath, int mutationIndex0, int mutationIndex1)
     newPath[mutationIndex1] = aux;
 }
 
+/**
+ * @brief Tenta mutar um indivíduo algumas vezes para que ele se torne melhor.
+ *
+ * @param individual Indivíduo a ser mutado.
+ * @param numVertices Número de vértices do grafo.
+ * @param numOfTries Número de tentativas.
+ */
 void GeneticAlgorithm::mutateIndividual(Individual &individual, int numVertices,
                                         int numOfTries = 3)
 {
@@ -55,7 +76,16 @@ void GeneticAlgorithm::mutateIndividual(Individual &individual, int numVertices,
     }
 }
 
-bool GeneticAlgorithm::mutatePopulation(vector<Individual> &population,
+/**
+ * @brief Faz as mutações em toda a população baseando-se na taxa de mutação.
+ *
+ * @param population População do algoritmo.
+ * @param mutationRate Taxa de mutação.
+ * @param numVertices Número de vértices do grafo.
+ * @param bestPath Ao mutar a população, esse indivíduo se transformará no novo
+ * melhor indivíduo dela.
+ */
+void GeneticAlgorithm::mutatePopulation(vector<Individual> &population,
                                         int mutationRate, int numVertices,
                                         Individual &bestPath)
 {
@@ -72,6 +102,12 @@ bool GeneticAlgorithm::mutatePopulation(vector<Individual> &population,
         }
 }
 
+/**
+ * @brief Cria um caminho hamiltoniano aleatório.
+ *
+ * @param numVertices Número de vértices do grafo.
+ * @param cities Vetor que receberá esse caminho.
+ */
 void GeneticAlgorithm::createRandomPath(int numVertices, vector<int> &cities)
 {
     int city;
@@ -92,11 +128,51 @@ void GeneticAlgorithm::createRandomPath(int numVertices, vector<int> &cities)
     }
 }
 
-bool pathsAreEqual(vector<int> &path0, vector<int> &path1, int pathSize)
+/**
+ * @brief Cria um caminho para um indivíduo e checa se já existe outro indivíduo
+ * com o mesmo caminho.
+ *
+ * @param numVertices Número de vértices do grafo.
+ * @param i Variável de controle do loop externo. Representa também o número de
+ * indivíduos gerados com sucesso - 1.
+ * @param individual Indivíduo que receberá o novo caminho.
+ * @param population População do algoritmo.
+ *
+ * @return true caso o indivíduo é válido, ou seja, não existe outro com o mesmo
+ * caminho.
+ * @return false caso contrário.
+ */
+bool GeneticAlgorithm::createIndividual(int numVertices, int i,
+                                        Individual &individual,
+                                        vector<Individual> &population)
 {
-    return memcmp(path0.data(), path1.data(), sizeof(int) * pathSize) == 0;
+    createRandomPath(numVertices, individual.cities);
+
+    auto searchEnd = population.begin() + i;
+    // Check if createRandomPath has generated a path that already exists
+    // in the population
+    auto individualPtr =
+        find_if(population.begin(), searchEnd,
+                [&individual, numVertices](Individual &currentIndividual) {
+                    return equal(individual.cities.begin(), individual.cities.end(),
+                                 currentIndividual.cities.begin());
+                });
+
+    return individualPtr == searchEnd;
 }
 
+/**
+ * @brief Gera uma população inicial para o algoritmo genético. Cada indivíduo
+ * será uma tupla (caminho, custoDoCaminho) onde 'caminho' é um vetor com os
+ * índices dos vértices do caminho hamiltoniano e o 'custoDoCaminho' é
+ * autoexplicativo.
+ *
+ * @param population Vetor que receberá a população.
+ * @param populationSize Tamanho da população.
+ * @param numVertices Número de vértices que o grafo tem.
+ * @param bestPath Ao gerar a população, esse parâmetro será populado com o melhor
+ * indivíduo até o momento.
+ */
 void GeneticAlgorithm::createRandomPopulation(vector<Individual> &population,
                                               int populationSize, int numVertices,
                                               Individual &bestPath)
@@ -106,22 +182,9 @@ void GeneticAlgorithm::createRandomPopulation(vector<Individual> &population,
     for (size_t i = 0; i < populationSize; i++)
     {
         Individual &individual = population[i];
-        do
-        {
-            createRandomPath(numVertices, individual.cities);
-
-            // Check if createRandomPath has
-            // generated a path that already
-            // exists in the population
-            individualPtr = find_if(
-                population.begin(), population.begin() + i,
-                [&individual, numVertices](Individual &currentIndividual) {
-                    return pathsAreEqual(individual.cities, currentIndividual.cities,
-                                         numVertices);
-                });
-
-            if (individualPtr == population.begin() + i) break;
-        } while (true);
+        // clang-format off
+        while (!createIndividual(numVertices, i, individual, population));
+        // clang-format on
 
         individual.pathCost = sumPath(individual.cities.data());
 
@@ -134,8 +197,9 @@ void GeneticAlgorithm::createRandomPopulation(vector<Individual> &population,
 }
 
 /**
- * Based on
- * https://www.geeksforgeeks.org/traveling-salesman-problem-using-genetic-algorithm/
+ * @brief Define o número de gerações, o tamanho da população e a taxa de mutação
+ * para o algoritmo genético. Depois, cria a população de forma aleatória
+ * e começa a realizar mutações para cada geração.
  */
 void GeneticAlgorithm::run()
 {
